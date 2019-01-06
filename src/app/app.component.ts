b6886593d6968680a27900285f56e9d2495d095b
Pradeep James <pjm123mail-github@yahoo.co.in>
2019-01-06 07:21:18 -0600

    Code Refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { ImageService } from './services/image.service';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,6 @@ export class AppComponent {
   canvasMaxHeight = 300;
   canvasMaxWidth = 500;
   inputImageData: ImageData;
-  greyscaleImageData: ImageData;
 
   fileSelectionHandler(e): void {
     if (e.target.files && e.target.files.length > 0) {
@@ -29,8 +29,8 @@ export class AppComponent {
   }
 
   detectEdgesClickHandler(): void {
-    if (this.greyscaleImageData) {
-      this.detectEdges(this.greyscaleImageData);
+    if (this.inputImageData) {
+      this.detectEdges(this.inputImageData);
     }
   }
 
@@ -63,119 +63,30 @@ export class AppComponent {
   }
 
   private convertToGreyscale(imageData: ImageData): void {
-    const data = imageData.data;
-    for (let i = 0; i < data.length; i += 4) {
-      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-      data[i] = avg; // red
-      data[i + 1] = avg; // green
-      data[i + 2] = avg; // blue
-    }
-    const canvas = this.greyscaleImageCanvas.nativeElement;
-    canvas.height = imageData.height;
-    canvas.width = imageData.width;
-    const ctx = canvas.getContext('2d');
-    ctx.putImageData(imageData, 0, 0);
-    this.greyscaleImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const greyscaleImageData: ImageData = ImageService.convertToGreyscale(imageData);
+    ImageService.paintImageDateOnCanvas(greyscaleImageData, this.greyscaleImageCanvas.nativeElement);
   }
 
   private detectEdges(imageData: ImageData): void {
-    const data = imageData.data;
-    const pixelMatrix: number[][] = [];
-    for (let i = 0; i < imageData.height; i++) {
-      const pixelRow: number[] = [];
-      for (let j = 0; j < imageData.width; j++) {
-        pixelRow.push(data[(i * imageData.width * 4) + (j * 4)]);
-      }
-      pixelMatrix.push(pixelRow);
-    }
     const horizontalPassFilter: number[][] = [
       [-1, 0, 1],
       [-2, 0, 2],
       [-1, 0, 1]
     ];
-    // const filter: number[][] = [
-    //   [3, 0, -3],
-    //   [10, 0, -10],
-    //   [3, 0, -3]
-    // ];
-    const horizontalPassFilteredPixelMatrix = this.applyFilter(pixelMatrix, horizontalPassFilter);
-    for (let i = 0; i < imageData.height; i++) {
-      for (let j = 0; j < imageData.width; j++) {
-        const index = (i * imageData.width * 4) + (j * 4);
-        const value = horizontalPassFilteredPixelMatrix[i][j];
-        data[index] = value; // red
-        data[index + 1] = value; // green
-        data[index + 2] = value; // blue
-      }
-    }
-    const horizontalCanvas = this.horizontalPassEdgeImageCanvas.nativeElement;
-    horizontalCanvas.height = imageData.height;
-    horizontalCanvas.width = imageData.width;
-    const horizontalCtx = horizontalCanvas.getContext('2d');
-    horizontalCtx.putImageData(imageData, 0, 0);
-
     const verticalPassFilter: number[][] = [
       [-1, -2, -1],
       [0, 0, 0],
       [1, 2, 1]
     ];
-    const verticalPassFilteredPixelMatrix = this.applyFilter(pixelMatrix, verticalPassFilter);
-    for (let i = 0; i < imageData.height; i++) {
-      for (let j = 0; j < imageData.width; j++) {
-        const index = (i * imageData.width * 4) + (j * 4);
-        const value = verticalPassFilteredPixelMatrix[i][j];
-        data[index] = value; // red
-        data[index + 1] = value; // green
-        data[index + 2] = value; // blue
-      }
-    }
-    const verticalCanvas = this.verticalPassEdgeImageCanvas.nativeElement;
-    verticalCanvas.height = imageData.height;
-    verticalCanvas.width = imageData.width;
-    const verticalCtx = verticalCanvas.getContext('2d');
-    verticalCtx.putImageData(imageData, 0, 0);
 
-    const magnitudeMatrix = this.calculateMagnitudeMatrix(horizontalPassFilteredPixelMatrix, verticalPassFilteredPixelMatrix);
-    for (let i = 0; i < imageData.height; i++) {
-      for (let j = 0; j < imageData.width; j++) {
-        const index = (i * imageData.width * 4) + (j * 4);
-        const value = magnitudeMatrix[i][j];
-        data[index] = value; // red
-        data[index + 1] = value; // green
-        data[index + 2] = value; // blue
-      }
-    }
-    const magnitudeCanvas = this.magnitudeImageCanvas.nativeElement;
-    magnitudeCanvas.height = imageData.height;
-    magnitudeCanvas.width = imageData.width;
-    const magnitudeCtx = magnitudeCanvas.getContext('2d');
-    magnitudeCtx.putImageData(imageData, 0, 0);
+    const horizontalPassImageData: ImageData = ImageService.applyFilterToImageData(imageData, horizontalPassFilter);
+    ImageService.paintImageDateOnCanvas(horizontalPassImageData, this.horizontalPassEdgeImageCanvas.nativeElement);
+
+    const verticalPassImageData: ImageData = ImageService.applyFilterToImageData(imageData, verticalPassFilter);
+    ImageService.paintImageDateOnCanvas(verticalPassImageData, this.verticalPassEdgeImageCanvas.nativeElement);
+
+    const multiplePassImageData: ImageData = ImageService.applyFiltersToImageData(imageData, horizontalPassFilter, verticalPassFilter);
+    ImageService.paintImageDateOnCanvas(multiplePassImageData, this.magnitudeImageCanvas.nativeElement);
   }
 
-  private applyFilter(pixelMatrix: number[][], filter: number[][]): number[][] {
-    const filteredPixelMatrix = JSON.parse(JSON.stringify(pixelMatrix));
-    for (let i = 1; i < (pixelMatrix.length - 1); i++) {
-      for (let j = 1; j < (pixelMatrix[i].length - 1); j++) {
-        const filteredValue: number =
-          filter[0][0] * pixelMatrix[i - 1][j - 1]
-          + filter[1][0] * pixelMatrix[i][j - 1]
-          + filter[2][0] * pixelMatrix[i + 1][j - 1]
-          + filter[0][2] * pixelMatrix[i - 1][j + 1]
-          + filter[1][2] * pixelMatrix[i][j + 1]
-          + filter[2][2] * pixelMatrix[i + 1][j + 1];
-        filteredPixelMatrix[i][j] = filteredValue;
-      }
-    }
-    return filteredPixelMatrix;
-  }
-
-  private calculateMagnitudeMatrix(matrix1: number[][], matrix2: number[][]): number[][] {
-    const resultMatrix: number[][] = JSON.parse(JSON.stringify(matrix1));
-    for (let i = 0; i < matrix1.length; i++) {
-      for (let j = 0; j < matrix1[0].length; j++) {
-        resultMatrix[i][j] = Math.sqrt((matrix1[i][j] * matrix1[i][j]) + (matrix2[i][j] * matrix2[i][j]));
-      }
-    }
-    return resultMatrix;
-  }
 }
---
+++ src/app/services/image.service.ts
@@ -0,0 +1,106 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+  constructor() { }
+
+  static convertToGreyscale(imageData: ImageData): ImageData {
+    const imageDataClone: ImageData = new ImageData(
+      new Uint8ClampedArray(imageData.data),
+      imageData.width,
+      imageData.height
+    );
+    const data = imageDataClone.data;
+    for (let i = 0; i < data.length; i += 4) {
+      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+      data[i] = avg; // red
+      data[i + 1] = avg; // green
+      data[i + 2] = avg; // blue
+    }
+    return imageDataClone;
+  }
+
+  static paintImageDateOnCanvas(imageData: ImageData, canvas: HTMLCanvasElement): void {
+    canvas.height = imageData.height;
+    canvas.width = imageData.width;
+    const ctx = canvas.getContext('2d');
+    ctx.putImageData(imageData, 0, 0);
+  }
+
+  static applyFilterToImageData(imageData: ImageData, filter: number[][]): ImageData {
+    const imageDataClone: ImageData = new ImageData(
+      new Uint8ClampedArray(imageData.data),
+      imageData.width,
+      imageData.height
+    );
+    const pixelMatrix = ImageService.convertImageDataTo2DPixelMatrix(imageDataClone);
+    const filteredPixelMatrix = JSON.parse(JSON.stringify(pixelMatrix));
+    for (let i = 1; i < (pixelMatrix.length - 1); i++) {
+      for (let j = 1; j < (pixelMatrix[i].length - 1); j++) {
+        filteredPixelMatrix[i][j] = filter[0][0] * pixelMatrix[i - 1][j - 1]
+          + filter[1][0] * pixelMatrix[i][j - 1]
+          + filter[2][0] * pixelMatrix[i + 1][j - 1]
+          + filter[0][2] * pixelMatrix[i - 1][j + 1]
+          + filter[1][2] * pixelMatrix[i][j + 1]
+          + filter[2][2] * pixelMatrix[i + 1][j + 1];
+      }
+    }
+    ImageService.translate2DPixelMatrixToImageData(filteredPixelMatrix, imageDataClone);
+    return imageDataClone;
+  }
+
+  static applyFiltersToImageData(imageData: ImageData,
+                                 horizontalFilter: number[][],
+                                 verticalFilter: number[][]): ImageData {
+    const imageDataClone: ImageData = new ImageData(
+      new Uint8ClampedArray(imageData.data),
+      imageData.width,
+      imageData.height
+    );
+    const horizontalPassPixelMatrix = ImageService.convertImageDataTo2DPixelMatrix(
+      ImageService.applyFilterToImageData(imageDataClone, horizontalFilter));
+    const verticalPassPixelMatrix = ImageService.convertImageDataTo2DPixelMatrix(
+      ImageService.applyFilterToImageData(imageDataClone, verticalFilter));
+
+    const resultMatrix: number[][] = JSON.parse(JSON.stringify(horizontalPassPixelMatrix));
+    for (let i = 0; i < horizontalPassPixelMatrix.length; i++) {
+      for (let j = 0; j < horizontalPassPixelMatrix[0].length; j++) {
+        resultMatrix[i][j] = Math.sqrt(
+          (horizontalPassPixelMatrix[i][j] * horizontalPassPixelMatrix[i][j])
+          + (verticalPassPixelMatrix[i][j] * verticalPassPixelMatrix[i][j])
+        );
+      }
+    }
+    ImageService.translate2DPixelMatrixToImageData(resultMatrix, imageDataClone);
+    return imageDataClone;
+  }
+
+  private static convertImageDataTo2DPixelMatrix(imageData: ImageData): number[][] {
+    const data = imageData.data;
+    const pixelMatrix: number[][] = [];
+    for (let i = 0; i < imageData.height; i++) {
+      const pixelRow: number[] = [];
+      for (let j = 0; j < imageData.width; j++) {
+        pixelRow.push(data[(i * imageData.width * 4) + (j * 4)]);
+      }
+      pixelMatrix.push(pixelRow);
+    }
+    return pixelMatrix;
+  }
+
+  private static translate2DPixelMatrixToImageData(pixelMatrix: number[][], imageData: ImageData): void {
+    const data = imageData.data;
+    for (let i = 0; i < imageData.height; i++) {
+      for (let j = 0; j < imageData.width; j++) {
+        const index = (i * imageData.width * 4) + (j * 4);
+        const value = pixelMatrix[i][j];
+        data[index] = value; // red
+        data[index + 1] = value; // green
+        data[index + 2] = value; // blue
+      }
+    }
+  }
+}
