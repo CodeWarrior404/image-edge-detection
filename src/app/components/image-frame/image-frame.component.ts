059b79037dae6b778968c7b224ed5dc7b0a44800
CodeWarrior404 <pjm123mail-github@yahoo.co.in>
2019-01-08 20:54:36 -0600

    Converted project from css to scss

--- src/app/components/image-frame/image-frame.component.ts
+++ src/app/components/image-frame/image-frame.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-image-frame',
   templateUrl: './image-frame.component.html',
-  styleUrls: ['./image-frame.component.css']
+  styleUrls: ['./image-frame.component.scss']
 })
 export class ImageFrameComponent implements OnInit {
   @Input() title: string;
